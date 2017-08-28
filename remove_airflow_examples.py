4388c3c9c6d26f2424f63aa16701fd19c5df22ba
Stanley Ndagi <ndagis@gmail.com>
2017-08-28 18:58:52 +0300

    [Feature #149582441] Add docker-compose option for development

--- remove_airflow_examples.py
+++ remove_airflow_examples.py
@@ -2,7 +2,7 @@ import os
 import sys
 import fileinput
 
-file = 'airflow.cfg'
+file = os.getenv('AIRFLOW_HOME') + '/airflow.cfg'
 
 for i, line in enumerate(fileinput.input(file, inplace=1)):
     sys.stdout.write(line.replace('load_examples = True',
