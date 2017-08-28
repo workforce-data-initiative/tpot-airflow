c0b81320718f9088c75d1622650addb8ab66ef2f
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-29 00:00:44 +0300

    refactor init hook

--- habitat/config/helpers.sh
+++ habitat/config/helpers.sh
@@ -4,14 +4,14 @@ check_user_exists() {
     echo $(id -u $1 > /dev/null 2>&1; echo $?)
 }
 
-delete_db_superuser() {
+delete_airflow_user() {
     userdel --force --remove {{cfg.superuser.name}}
 }
 
-create_db_superuser() {
+create_airflow_user() {
     if [ $(check_user_exists "{{cfg.superuser.name}}") -eq 1 ]; then
-        echo "Create airflow superuser"
-        useradd --user-group --create-home {{cfg.superuser.name}}
+        echo "Creating airflow superuser"
+        useradd --user-group {{cfg.superuser.name}}
     fi
 
     echo "User-$(id {{cfg.superuser.name}})"
