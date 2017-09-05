06ab1e4e091eedd0e9bd6613f34bb4c3f2fb684c
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-09-05 15:28:37 +0300

    [#149581677] add airflow resetter to helper script

--- habitat/config/helpers.sh
+++ habitat/config/helpers.sh
@@ -1,18 +1,6 @@
 # Helper functions to create airflow user
 
-check_user_exists() {
-    echo $(id -u $1 > /dev/null 2>&1; echo $?)
+reset_airflow_db() {
+    airflow resetdb --yes
 }
 
-delete_airflow_user() {
-    userdel --force --remove {{cfg.superuser.name}}
-}
-
-create_airflow_user() {
-    if [ $(check_user_exists "{{cfg.superuser.name}}") -eq 1 ]; then
-        echo "Creating airflow superuser"
-        useradd --user-group {{cfg.superuser.name}}
-    fi
-
-    echo "User-$(id {{cfg.superuser.name}})"
-}
