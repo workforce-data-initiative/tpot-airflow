cd8c2e79a6a053729ad4a8a6924f862590de6103
The Things They Coded <hello@thethingstheycoded.com>
2017-12-13 10:01:51 -0600

    added sample DAG node to grab file from s3

--- dags/s3_dag_test.py
+++ dags/s3_dag_test.py
@@ -7,10 +7,13 @@ https://stackoverflow.com/questions/39997714/airflow-s3-connection-using-ui/4077
 """
 
 from airflow import DAG
-from airflow.operators import SimpleHttpOperator, HttpSensor, EmailOperator
-from airflow.operators import BashOperator
-from airflow.operators import S3KeySensor
+#from airflow.operators import SimpleHttpOperator, HttpSensor, EmailOperator
+#from airflow.operators import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.sensors import S3KeySensor
 from datetime import datetime, timedelta
+from airflow.hooks.S3_hook import S3Hook
+import boto
 
 default_args = {
     'owner': 'airflow',
@@ -23,21 +26,31 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
+def grab_file():
+    s3_conn_id='my_conn_S3'
+    s3 = S3Hook(s3_conn_id)
+
+    key_label = "file-to-watch-3.txt"
+    key = s3.get_key(key_label, 'superconductive-airflow-bucket')
+    key_string = key.get_contents_as_string()
+
+    return key_string
+
 dag = DAG('s3_dag_test', default_args=default_args, schedule_interval= '@once')
 
-t1 = BashOperator(
-    task_id='bash_test',
-    bash_command='echo "hello, it should work" > s3_conn_test.txt',
+file_processor = PythonOperator(
+    task_id='grab_file_from_s3',
+    python_callable=grab_file,
     dag=dag)
 
-sensor = S3KeySensor(
+file_trigger = S3KeySensor(
     task_id='check_s3_for_file_in_s3',
     bucket_key='file-to-watch-*',
     wildcard_match=True,
-    bucket_name='airflow-bucket',
+    bucket_name='superconductive-airflow-bucket',
     s3_conn_id='my_conn_S3',
     timeout=18*60*60,
     poke_interval=120,
     dag=dag)
 
-t1.set_upstream(sensor)
+file_processor.set_upstream(file_trigger)
