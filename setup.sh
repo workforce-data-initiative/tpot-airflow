98a873c959847b02138a22c73abdebdade2c82ba
Stanley Ndagi <ndagis@gmail.com>
2018-03-21 12:17:50 +0300

    Restructure files/ folder structure

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export AIRFLOW_HOME=$(pwd)
 airflow initdb
-python remove_airflow_examples.py
+python config/remove_airflow_examples.py
 airflow resetdb -y
-python customize_dashboard.dev.py
+python config/customize_dashboard.dev.py
 airflow webserver
