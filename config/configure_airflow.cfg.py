9d7ae4bc83e360163e4deafdc694929a5bfd9c46
Stanley Ndagi <ndagis@gmail.com>
2018-04-04 20:48:42 +0300

    Add Google Auth

--- config/configure_airflow.cfg.py
+++ config/configure_airflow.cfg.py
@@ -5,17 +5,26 @@ import logging
 from cryptography.fernet import Fernet
 
 FERNET_KEY = Fernet.generate_key().decode()
-file = os.getenv('AIRFLOW_HOME') + '/airflow.cfg'
+AIRFLOW_HOME = os.getenv('AIRFLOW_HOME')
+file = AIRFLOW_HOME + '/airflow.cfg'
 
 for i, line in enumerate(fileinput.input(file, inplace=1)):
     sys.stdout.write(line.replace('load_examples = True',
                                   'load_examples = False'))
 for i, line in enumerate(fileinput.input(file, inplace=1)):
-    sys.stdout.write(line.replace('#AIRFLOW_HOME#',
-                                  os.getenv('AIRFLOW_HOME')))
-for i, line in enumerate(fileinput.input(file, inplace=1)):
     sys.stdout.write(line.replace('#FERNET_KEY#', FERNET_KEY))
 
+for i, line in enumerate(fileinput.input(file, inplace=1)):
+    sys.stdout.write(line.replace('#AIRFLOW_HOME#', AIRFLOW_HOME))
+
+for i, line in enumerate(fileinput.input(file, inplace=1)):
+    sys.stdout.write(line.replace('#GOOGLE_CLIENT_ID#',
+                                  os.getenv('GOOGLE_CLIENT_ID')))
+
+for i, line in enumerate(fileinput.input(file, inplace=1)):
+    sys.stdout.write(line.replace('#GOOGLE_CLIENT_SECRET#',
+                                  os.getenv('GOOGLE_CLIENT_SECRET')))
 
 logging.info("Removing Example DAGs")
-logging.info("Setting AIRFLOW_HOME and FERNET_KEY in airflow.cfg")
+logging.info("Setting AIRFLOW_HOME, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET and \
+FERNET_KEY in airflow.cfg")
